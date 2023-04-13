afb61f00dff9ac74948c41b80ad36c8aaf63cd58
albucatalina <albuelena1905@gmail.com>
2023-04-13 12:06:47 +0300

    Add project files.

---
+++ ValvesControl/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿
+namespace ValvesControl
+{
+    partial class ValvesControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.consoleLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.electricalSchemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.relaysToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hystoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testsHystoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.valveComboBox = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.currentLabel = new System.Windows.Forms.Label();
+            this.voltageLabel = new System.Windows.Forms.Label();
+            this.currentTextBox = new System.Windows.Forms.TextBox();
+            this.voltagTtextBox = new System.Windows.Forms.TextBox();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consoleTextBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // consoleLabel
+            // 
+            this.consoleLabel.AutoSize = true;
+            this.consoleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.consoleLabel.Location = new System.Drawing.Point(12, 325);
+            this.consoleLabel.Name = "consoleLabel";
+            this.consoleLabel.Size = new System.Drawing.Size(77, 20);
+            this.consoleLabel.TabIndex = 1;
+            this.consoleLabel.Text = "Console";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Orange;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.electricalSchemaToolStripMenuItem,
+            this.relaysToolStripMenuItem,
+            this.hystoryToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.testsHystoryToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(693, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // electricalSchemaToolStripMenuItem
+            // 
+            this.electricalSchemaToolStripMenuItem.Name = "electricalSchemaToolStripMenuItem";
+            this.electricalSchemaToolStripMenuItem.Size = new System.Drawing.Size(139, 24);
+            this.electricalSchemaToolStripMenuItem.Text = "Electrical Schema";
+            // 
+            // relaysToolStripMenuItem
+            // 
+            this.relaysToolStripMenuItem.Name = "relaysToolStripMenuItem";
+            this.relaysToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.relaysToolStripMenuItem.Text = "Relays";
+            // 
+            // hystoryToolStripMenuItem
+            // 
+            this.hystoryToolStripMenuItem.Name = "hystoryToolStripMenuItem";
+            this.hystoryToolStripMenuItem.Size = new System.Drawing.Size(70, 24);
+            this.hystoryToolStripMenuItem.Text = "History";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // testsHystoryToolStripMenuItem
+            // 
+            this.testsHystoryToolStripMenuItem.Name = "testsHystoryToolStripMenuItem";
+            this.testsHystoryToolStripMenuItem.Size = new System.Drawing.Size(14, 24);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 474);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Clean Console";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.valveComboBox);
+            this.groupBox1.Location = new System.Drawing.Point(16, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(211, 268);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Test Config";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 205);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 40);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(18, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Time";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(18, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Short circuit type";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(18, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Valve";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(74, 161);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(81, 22);
+            this.numericUpDown1.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(17, 113);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(166, 24);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // valveComboBox
+            // 
+            this.valveComboBox.FormattingEnabled = true;
+            this.valveComboBox.Location = new System.Drawing.Point(17, 53);
+            this.valveComboBox.Name = "valveComboBox";
+            this.valveComboBox.Size = new System.Drawing.Size(166, 24);
+            this.valveComboBox.TabIndex = 6;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.currentLabel);
+            this.groupBox2.Controls.Add(this.voltageLabel);
+            this.groupBox2.Controls.Add(this.currentTextBox);
+            this.groupBox2.Controls.Add(this.voltagTtextBox);
+            this.groupBox2.Location = new System.Drawing.Point(299, 54);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(335, 129);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Measurement";
+            // 
+            // currentLabel
+            // 
+            this.currentLabel.AutoSize = true;
+            this.currentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.currentLabel.Location = new System.Drawing.Point(268, 49);
+            this.currentLabel.Name = "currentLabel";
+            this.currentLabel.Size = new System.Drawing.Size(34, 32);
+            this.currentLabel.TabIndex = 3;
+            this.currentLabel.Text = "A";
+            // 
+            // voltageLabel
+            // 
+            this.voltageLabel.AutoSize = true;
+            this.voltageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.voltageLabel.Location = new System.Drawing.Point(119, 49);
+            this.voltageLabel.Name = "voltageLabel";
+            this.voltageLabel.Size = new System.Drawing.Size(34, 32);
+            this.voltageLabel.TabIndex = 2;
+            this.voltageLabel.Text = "V";
+            // 
+            // currentTextBox
+            // 
+            this.currentTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.currentTextBox.Location = new System.Drawing.Point(168, 43);
+            this.currentTextBox.Multiline = true;
+            this.currentTextBox.Name = "currentTextBox";
+            this.currentTextBox.ReadOnly = true;
+            this.currentTextBox.Size = new System.Drawing.Size(94, 38);
+            this.currentTextBox.TabIndex = 1;
+            this.currentTextBox.Text = "+0.00";
+            // 
+            // voltagTtextBox
+            // 
+            this.voltagTtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.voltagTtextBox.Location = new System.Drawing.Point(19, 43);
+            this.voltagTtextBox.Multiline = true;
+            this.voltagTtextBox.Name = "voltagTtextBox";
+            this.voltagTtextBox.ReadOnly = true;
+            this.voltagTtextBox.Size = new System.Drawing.Size(94, 38);
+            this.voltagTtextBox.TabIndex = 0;
+            this.voltagTtextBox.Text = "+0.00";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // consoleTextBox
+            // 
+            this.consoleTextBox.Location = new System.Drawing.Point(16, 348);
+            this.consoleTextBox.Multiline = true;
+            this.consoleTextBox.Name = "consoleTextBox";
+            this.consoleTextBox.ReadOnly = true;
+            this.consoleTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.consoleTextBox.Size = new System.Drawing.Size(645, 120);
+            this.consoleTextBox.TabIndex = 8;
+            // 
+            // ValvesControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.ClientSize = new System.Drawing.Size(693, 528);
+            this.Controls.Add(this.consoleTextBox);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.consoleLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ValvesControl";
+            this.RightToLeftLayout = true;
+            this.Text = "Valves control";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label consoleLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem electricalSchemaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relaysToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem testsHystoryToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox valveComboBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label currentLabel;
+        private System.Windows.Forms.Label voltageLabel;
+        private System.Windows.Forms.TextBox currentTextBox;
+        private System.Windows.Forms.TextBox voltagTtextBox;
+        private System.Windows.Forms.ToolStripMenuItem hystoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.TextBox consoleTextBox;
+    }
+}
+
